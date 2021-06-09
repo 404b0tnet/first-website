c8bda5a378d997df67f2fc41096c253ced2f35b9
404b0tnet <jsmith728.js@gmail.com>
2021-06-09 09:26:26 -0400

    removed bad image

--- first-test-site/scripts/main.js
+++ first-test-site/scripts/main.js
@@ -1,13 +1,13 @@
 // change images on click
 let myImage = document.querySelector('img');
 
-myImage.onclick = function() {
-    let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox.jfif') {
-      myImage.setAttribute('src','images/irlFirefox.jpg');
-    } else {
-      myImage.setAttribute('src','images/firefox.jfif');
-    }
+myImage.onclick = function () {
+  let mySrc = myImage.getAttribute('src');
+  if (mySrc === 'images/firefox.jfif') {
+    myImage.setAttribute('src', 'images/irlFirefox.jpg');
+  } else {
+    myImage.setAttribute('src', 'images/firefox.jfif');
+  }
 }
 
 
@@ -18,7 +18,7 @@ let myHeading = document.querySelector('h1');
 // create user "log in" button
 function setUserName() {
   let myName = prompt('Please enter your name.');
-  if(!myName) {
+  if (!myName) {
     setUserName();
   } else {
     localStorage.setItem('name', myName);
@@ -26,14 +26,13 @@ function setUserName() {
   }
 }
 
-if(!localStorage.getItem('name')) {
+if (!localStorage.getItem('name')) {
   setUserName();
 } else {
   let storedName = localStorage.getItem('name');
   myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
 }
 
-myButton.onclick = function() {
+myButton.onclick = function () {
   setUserName();
 }
-  
